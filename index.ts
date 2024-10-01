3bf798ff2c6da219832a5a4b892faf112f1f9f2e
Ian Beddie <ian.beddie@mths.ca>
2024-10-01 13:24:16 +0000

    added working number game

--- index.ts
+++ index.ts
@@ -7,35 +7,36 @@
 */
 
 import { createPrompt } from 'bun-promptx'
-import random from 'random'
 
-//declare variables
+//  declare variables
+const whilePlaceHolder = 1
 let loopCounter = 1
 let correctNumber = 0
 let userGuess = 0
 
-correctNumber = random.int((min = 1), (max = 6))
+correctNumber = Math.floor(Math.random() * 6) + 1
 
-while (1 == 1) {
-  userGuessString = createPrompt('Guess a number between 1 and 6:')
-if (isNaN(parseINT(userGuessString))) {
-  console.log('You did not enter a number.')
-} else {
-  userGuess = parseINT(userGuessString)
-if (userGuess == correctNumber) {
-  console.log('You win!!')
-  console.log(`It took you ${loopCounter} guesses. `)
-break;
-} else if (userGuess < 1 || userGuess >6) {
-  console.log('you did not guess within range')
-  loopCounter++
-} else if (userGuess < correctNumber) {
-  print("You guessed too low, try again.")
-  loopCounter++
-} else if (userGuess > correctNumber) {
-  print("You guessed too high, try again.")
-  loopCounter++
+while (whilePlaceHolder === 1) {
+  const userGuessString = createPrompt('Guess a number between 1 and 6:')
+  if (isNaN(parseInt(userGuessString.value))) {
+    console.log('You did not enter a number.')
+  } else {
+    userGuess = parseInt(userGuessString.value)
+    if (userGuess === correctNumber) {
+      console.log('You win!!')
+      console.log(`It took you ${loopCounter} guesses. `)
+      break
+    } else if (userGuess < 1 || userGuess > 6) {
+      console.log('you did not guess within range')
+      loopCounter++
+    } else if (userGuess < correctNumber) {
+      console.log('You guessed too low, try again.')
+      loopCounter++
+    } else if (userGuess > correctNumber) {
+      console.log('You guessed too high, try again.')
+      loopCounter++
+    }
+  }
 }
-}
-}
+
 console.log('\nDone.')
